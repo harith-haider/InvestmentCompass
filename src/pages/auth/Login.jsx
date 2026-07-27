66f9841aa5992aba0b9d55e185cf479b6392ada4
harith-haider <harithhaideritse24@student.uobabylon.edu.iq>
2026-07-27 22:02:21 +0300

    Update API endpoint to Railway live server

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     
     try {
       // إرسال طلب تسجيل الدخول للـ Backend المرفوع
-      const response = await fetch('', {
+      const response = await fetch('investmentcompassapi-production-8571.up.railway.app', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
